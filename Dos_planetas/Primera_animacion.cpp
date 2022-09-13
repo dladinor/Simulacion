65646463fec4ec197343a5bcbd634a0d25d2f2dd
dladinor <dladinor@github.com>
2022-09-13 11:42:40 -0500

    :zap: Codigo para animaciones dos planetas

---
+++ Dos_planetas/Primera_animacion.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (void){
+
+    cout<<"plot \"datos_dos_planetas.dat\" w l"<<endl;
+    cout<<"pause 3"<<endl;
+
+    return 0;
+}
--- Dos_planetas/dos_planetas_inter.cpp
+++ Dos_planetas/dos_planetas_inter.cpp
@@ -25,13 +25,15 @@ public:
   void SumeFuerza(vector3D F0);
   void Mueva_r(double dt, double coeficiente);
   void Mueva_V(double dt, double coeficiente);
-
+  void Dibujese(void);
   double Getx(void){return r.x();}; //Inline
   double Gety(void){return r.y();}; //Inline
   double Getz(void){return r.z();};//Inline
 
   friend class Colisionador;
 };
+
+
 void Cuerpo::Inicie(double x0,double y0, double z0,double Vx0,double Vy0, double Vz0, double m0,double R0){
   r.load(x0,y0,z0); V.load(Vx0,Vy0,Vz0); m=m0; R=R0;
 }
@@ -49,6 +51,10 @@ void Cuerpo::Mueva_r(double dt, double coeficiente){
 void Cuerpo::Mueva_V(double dt, double coeficiente){
    V+=F*(dt*coeficiente/m);
   
+}
+
+void Cuerpo::Dibujese(void){
+  cout<<" , "<<r.x()<<"+"<<R<<"*cos(t),"<<r.y()<<"+"<<R<<"*sin(t)";
 }
 //-------Clase colisionador----------
 class Colisionador{
@@ -77,8 +83,25 @@ void Colisionador::CalculeFuerzaEntre(Cuerpo & Planeta1, Cuerpo & Planeta2){
   Planeta1.SumeFuerza(F1); Planeta2.SumeFuerza(F1*(-1));
 }
 //----------- Funciones Globales -----------
+void InicieAnimacion(void){
+  cout<<"set terminal gif animate"<<endl; 
+  cout<<"set output 'Dos_planetas.gif'"<<endl;
+  cout<<"unset key"<<endl;
+  cout<<"set xrange[-12:12]"<<endl;
+  cout<<"set yrange[-12:12]"<<endl;
+  cout<<"set size ratio -1"<<endl;
+  cout<<"set parametric"<<endl;
+  cout<<"set trange [0:7]"<<endl;
+  cout<<"set isosamples 12"<<endl;  
+}
+void InicieCuadro(void){
+    cout<<"plot 0,0 ";
+}
+void TermineCuadro(void){
+    cout<<endl;
+}
 int main(){
-  double t,dt=1.0; int ii;
+  int ii;
   double m0=10, m1=1, r=11;
   double M = m0+m1, x0=-m1*r/M, x1=m0*r/M;
   double omega, T, V0, V1;
@@ -89,13 +112,25 @@ int main(){
   omega= sqrt(G*M*pow(r,-3.0)); 
   T = 2*M_PI/omega;
   V0=omega*x0, V1=omega*x1;
+  double t,tmax=10*T,dt=0.01; 
+  double tdibujo, tcuadro=T/500;
   
   //omega=sqrt(G/(r0*r0*r0)); V0=omega*r0; T=2*M_PI/omega;
   
   //------------(x0,y0,z0,Vx0,Vy0,Vz0,m0,R0)
   Planeta[0].Inicie(x0,0,0,0,V0,0,m0,0.5);
   Planeta[1].Inicie(x1,0,0,0,V1,0,m1,0.5);
-  for(t=0;t<1.1*T;t+=dt){
+
+  InicieAnimacion();
+
+  for(t=0, tdibujo=0;t<tmax;t+=dt,tdibujo+=dt){
+    //Dibujar
+    if(tdibujo>tcuadro){
+      InicieCuadro();
+      for(ii=0;ii<N;ii++) Planeta[ii].Dibujese();
+      TermineCuadro();
+      tdibujo=0;
+    }
     cout<<Planeta[1].Getx()<<"\t"<<Planeta[1].Gety()<<endl;
     for(ii=0;ii<N;ii++) {Planeta[ii].Mueva_r(dt,Zeta);}
     Newton.CalculeFuerzas(Planeta);
